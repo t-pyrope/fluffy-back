0912d4ae0700989d7fe91305cdb40517156edd18
Begaiym Adylbek kyzy <mrmldsky27@gmail.com>
2022-12-26 09:12:48 +0100

    Connect to MongoDB Atlas

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const config = {
     mongodb: {
-        uri: 'mongodb://localhost/fluffy'
-    }
+        uri: `mongodb+srv://fluffy-admin:${process.env.MONGO_DB_PASSWORD}@fluffy-back.mgfx43w.mongodb.net/?retryWrites=true&w=majority`,
+    },
 };
 
 module.exports = config;
